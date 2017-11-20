329a7ef14eb1154559e622be0aabf6dafcffc206
Alex Page <alexpage@paddle-mbp.local>
2017-11-20 22:13:12 +1100

    fixing api call

--- src/init.js
+++ src/init.js
@@ -26,7 +26,8 @@ if(process.argv.includes('-v') || process.argv.includes('--verbose')) {
 Log.welcome( `Black Lion: Starting the feast` );
 
 GetTotalPages( SETTINGS.get().api.commerce )
-	.then( totalPages => GetBulkData( SETTINGS.get().api.items, totalPages ) )
+	.then( totalPages => GetBulkData( SETTINGS.get().api.commerce, totalPages ) )
+	// then send data to db with timestamp
 	.then( data => Log.done( `The lions are full and go to sleep` ) )
 	.catch( error => Log.error( `The lions went hungry: ${ error }` ) );
 
